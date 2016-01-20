ed93647fa203a6383c3ae52536f8466cf5f7edf6
Adam Ahrens <adam.ahrens@smartthings.com>
2016-01-19 21:31:23 -0600

    Adding color to terminal for rspec

--- raceday/spec/spec_helper.rb
+++ raceday/spec/spec_helper.rb
@@ -19,6 +19,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 RSpec.configure do |config|
+
+  # Color the terminal output
+  config.color = true
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
