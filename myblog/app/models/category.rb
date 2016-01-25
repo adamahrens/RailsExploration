f9596d6beb5b01406c2e865d3e13b2e964960948
Adam Ahrens <adam.ahrens@smartthings.com>
2016-01-24 18:17:20 -0600

    Addings the associations

--- myblog/app/models/category.rb
+++ myblog/app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+	# Allows us access to the category name
+	attr_accessible :name
+
+	# A Category has many Posts
+	has_many :posts
 end
--- myblog/app/models/post.rb
+++ myblog/app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+	# Creates the getters and setters that can be set on Post model
+	attr_accessible :name, :body, :category_id, :author_id
+
+	# Post belongs to a single Category
+	belongs_to :category
 end
