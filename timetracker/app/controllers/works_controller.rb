4f5bad30f8d95de751607b7249dadccb609a1d34
Adam Ahrens <adam.ahrens@smartthings.com>
2016-02-04 22:44:25 -0600

    Adding a new Route for filtering Work

--- timetracker/app/controllers/works_controller.rb
+++ timetracker/app/controllers/works_controller.rb
@@ -1,8 +1,12 @@
 class WorksController < ApplicationController
 	def index
-		# Include prevents N+1 queries in the View
-		# @works.project.name would have caused an N+1 query
-		@works = Work.all.includes(:user).includes(:project)
+		if params[:days]
+			@works = Work.recent_days(params[:days]).order('date_performed desc').includes(:user).includes(:project)
+		else
+			# Include prevents N+1 queries in the View
+			# @works.project.name would have caused an N+1 query
+			@works = Work.all.includes(:user).includes(:project)
+		end
 	end
 
 	def show
--- timetracker/app/models/work.rb
+++ timetracker/app/models/work.rb
@@ -10,4 +10,10 @@ class Work < ActiveRecord::Base
   def date_not_in_future
   	date_performed < Time.now
   end
+
+  # self. means it's a class method. So Work.recent_days
+  def self.recent_days(number_of_days_ago)
+  	since_date = Time.now - number_of_days_ago.to_i.days
+  	where("date_performed > :since_date", since_date: since_date)
+  end
 end
