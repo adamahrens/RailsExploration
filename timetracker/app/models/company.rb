786970343fcd525b2a3abfd88844a23da0e1b747
Adam Ahrens <adam.ahrens@smartthings.com>
2016-02-04 22:22:44 -0600

    Adding controllers for Work, Companies, and Projects

--- timetracker/app/models/company.rb
+++ timetracker/app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ActiveRecord::Base
 	has_many :users
 	has_many :projects
 
-	validates_length_of :name, minimum: 5, 'Company name must be greater than 4 characters'
+	validates_length_of :name, minimum: 2, message: 'Company name must be greater than 2 characters'
 end
