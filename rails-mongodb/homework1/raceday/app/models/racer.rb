d676a2842dd8284ea5074f0041cd8ac702a4261d
Adam Ahrens <adam.ahrens@smartthings.com>
2016-04-21 21:28:38 -0500

    Adding all method for fetching Racers

--- rails-mongodb/homework1/raceday/app/models/racer.rb
+++ rails-mongodb/homework1/raceday/app/models/racer.rb
@@ -1,6 +1,7 @@
 class Racer
   include Mongoid::Document
 
+  # class methods
   def self.mongo_client
     Mongoid::Clients.default
   end
@@ -8,4 +9,18 @@ class Racer
   def self.collection
     self.mongo_client[:racers]
   end
+
+  def self.all(prototype={}, sort={ :number => 1 }, offset=0, limit=nil)
+    if prototype.empty?
+      return Racer.collection.find
+    else
+      if limit.nil?
+        return Racer.collection.find(prototype).sort(sort).skip(offset)
+      else
+        return Racer.collection.find(prototype).sort(sort).skip(offset).limit(limit)
+      end
+    end
+  end
+
+  # instance methods
 end
