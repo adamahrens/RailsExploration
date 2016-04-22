0921aa0a7f6222dc118ecd863cf0f8e930483162
Adam Ahrens <adam.ahrens@smartthings.com>
2016-04-22 10:08:18 -0500

    Implementing CRUD operations for Racer

--- rails-mongodb/homework1/raceday/app/models/racer.rb
+++ rails-mongodb/homework1/raceday/app/models/racer.rb
@@ -24,12 +24,47 @@ class Racer
     end
   end
 
-  def self.find
+  # Could be a string or BSON::ObjectId
+  def self.find(id)
+    # Find the Racer document if it exists
+    racer = Racer.collection.find(_id: Racer.convert_id_for_find(id)).first
+    return racer.nil? ? nil : Racer.new(racer)
+  end
+
+  def self.convert_id_for_find(id)
+    find_id = id
+    if id.is_a? String
+      find_id = BSON::ObjectId.from_string(id)
+    end
+    find_id
   end
 
   # instance methods
   def initialize(params={})
     @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+    set_racer(params)
+  end
+
+  def save
+    document = Racer.collection.insert_one(racer_hash)
+    @id = document.inserted_id.to_s
+  end
+
+  def update(params)
+    set_racer(params)
+    Racer.collection.find(_id: Racer.convert_id_for_find(@id)).update_one(params)
+  end
+
+  def destroy
+    Racer.collection.find(_id: Racer.convert_id_for_find(@id)).delete_one()
+  end
+
+  private
+  def racer_hash
+    return { number: @number, first_name: @first_name, last_name: @last_name, gender: @gender, group: @group, secs: @secs }
+  end
+
+  def set_racer(params)
     @number = params[:number].to_i
     @first_name = params[:first_name]
     @last_name = params[:last_name]
