def2258b527c7b0599c67c4767c9bf5dc0570096
Adam Ahrens <adam.ahrens@smartthings.com>
2016-06-06 17:50:13 -0500

    Adding attributes to Place

--- rails-mongodb/homework2/places/app/models/place.rb
+++ rails-mongodb/homework2/places/app/models/place.rb
@@ -1,6 +1,11 @@
 class Place
   include Mongoid::Document
 
+  attr_accessor :id                 # String
+  attr_accessor :formatted_address  # String
+  attr_accessor :location           # Point
+  attr_accessor :address_components # AddressComponent Array
+
   def self.mongo_client
     Mongoid::Clients.default
   end
@@ -13,5 +18,20 @@ class Place
     file = File.read(io_file)
     json_string = JSON.parse(file)
     self.collection.insert_many(json_string)
+  end
+
+  def self.convert_id(id)
+    id.is_a?(String) ? id : id.to_s 
+  end
+
+  def initialize(hash)
+    @id = Place.convert_id(hash[:_id])
+    @formatted_address = hash[:formatted_address]
+    @address_components = []
+    hash[:address_components].each { |address_hash|
+      @address_components << AddressComponent.new(address_hash)
+    }
+
+    @location = Point.new(hash[:geometry][:location])
   end
 end
