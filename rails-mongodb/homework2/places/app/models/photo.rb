ffd8283638f910dc8e90d4d3fc808ae4d066e2b0
Adam Ahrens <adam.ahrens@smartthings.com>
2016-06-14 08:09:34 -0500

    Adding query methods to Photo

--- rails-mongodb/homework2/places/app/models/photo.rb
+++ rails-mongodb/homework2/places/app/models/photo.rb
@@ -9,13 +9,32 @@ class Photo
     Mongoid::Clients.default
   end
 
+  def self.all(offset=0, limit=nil)
+    if limit.nil?
+      return Photo.mongo_client.database.fs.find.skip(offset).map { |doc| Photo.new(doc) }
+    else
+      return Photo.mongo_client.database.fs.find.skip(offset).limit(limit).map { |doc| Photo.new(doc) }
+    end
+  end
+
+  def self.find(id)
+    found_place = Photo.mongo_client.database.fs.find(_id: Photo.convert_id_for_find(id)).first
+    found_place.nil? ? nil : Photo.new(found_place)
+  end
+
+  def self.convert_id_for_find(id)
+    id.is_a?(String) ? BSON::ObjectId.from_string(id) : id
+  end
+
   def self.convert_id(id)
     id.is_a?(String) ? id : id.to_s
   end
 
-  def initialize(hash)
-    @id = Photo.convert_id(hash[:_id])
-    @location = Point.new(hash[:metadata][:location]) unless hash[:metadata][:location].nil?
+  def initialize(hash=nil)
+    unless hash.nil?
+      @id = Photo.convert_id(hash[:_id]) unless hash[:_id].nil?
+      @location = Point.new(hash[:metadata][:location]) unless hash[:metadata].nil? && hash[:metadata][:location].nil?
+    end
   end
 
   def persisted?
@@ -24,7 +43,12 @@ class Photo
 
   def save
     unless persisted?
-      
+      gps = EXIFR::JPEG.new(@contents).gps
+      @location = Point.new(lng: gps.longitude, lat: gps.latitude)
+      description = { content_type: "image/jpeg", metadata: { location: { coordinates: [gps.longitude, gps.latitude]}}}
+      grid_file = Mongo::Grid::File.new(@contents.read, description)
+      @contents.rewind
+      @id = Photo.mongo_client.database.fs.insert_one(grid_file)
     end
   end
 end
