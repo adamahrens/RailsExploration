6a5a76e599256585300228e99087a288f0a2dba1
Adam Ahrens <adam.ahrens@smartthings.com>
2016-08-16 18:36:58 -0500

    Fixing more rspec tests

--- rails-mongodb/homework2/places/app/models/photo.rb
+++ rails-mongodb/homework2/places/app/models/photo.rb
@@ -34,6 +34,7 @@ class Photo
     unless hash.nil?
       @id = Photo.convert_id(hash[:_id]) unless hash[:_id].nil?
       @location = Point.new(hash[:metadata][:location]) unless hash[:metadata].nil? && hash[:metadata][:location].nil?
+      @place_id = Photo.convert_id_for_find(hash[:metadata][:place])
     end
   end
 
@@ -53,14 +54,29 @@ class Photo
     place = Place.near(point, max_meters).limit(1).sort({ :location => 1 }).first[:_id]
   end
 
+  def place
+    Place.find(@place_id)
+  end
+
+  def place=(value)
+    @place_id = Photo.convert_id_for_find(value)
+  end
+
+  def self.find_photos_for_place(find_by)
+    place_id = Photo.convert_id_for_find(find_by)
+    Photo.mongo_client.database.fs.find(metadata: { place: place_id})
+  end
+
   def save
     unless persisted?
       gps = EXIFR::JPEG.new(@contents).gps
       @location = Point.new(lng: gps.longitude, lat: gps.latitude)
-      description = { content_type: "image/jpeg", metadata: { location: { coordinates: [gps.longitude, gps.latitude]}}}
+      description = { content_type: "image/jpeg", metadata: { location: { coordinates: [gps.longitude, gps.latitude] }, place: @place_id}}
       grid_file = Mongo::Grid::File.new(@contents.read, description)
       @contents.rewind
       @id = Photo.mongo_client.database.fs.insert_one(grid_file)
+    else
+      Photo.mongo_client.database.fs.find(_id: Photo.convert_id_for_find(id)).update_one(metadata: { location: { coordinates: [@location.longitude, @location.latitude] }, place: @place_id})
     end
   end
 end
