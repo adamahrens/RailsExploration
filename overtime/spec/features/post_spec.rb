bc21e70dc6d3c4df46deb62261bf0a62cc56a6d8
Adam Ahrens <adam.ahrens@smartthings.com>
2016-08-29 19:53:32 -0500

    First pass at CR of CRUD

--- overtime/spec/features/post_spec.rb
+++ overtime/spec/features/post_spec.rb
@@ -13,4 +13,21 @@ describe 'navigate' do
       expect(page).to have_content(/Overtime Requests/)
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'I need time off'
+      click_on 'Save'
+      expect(page).to have_content('I need time off')
+    end
+  end
 end
