abc6f40a9ca2704d2f4d6bf954dddb211a0caa45
Adam Ahrens <adam.ahrens@smartthings.com>
2016-08-30 19:52:27 -0500

    Adding some basic layout

--- overtime/app/helpers/application_helper.rb
+++ overtime/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def active?(path)
+    # Returns the active string if the current page is the same as the path
+    # Used for applying css to _navbar
+    "active" if current_page?(path)
+  end
 end
