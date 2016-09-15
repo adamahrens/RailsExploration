6614787c8c5ee1b1e097d4d2aac893efe6171ccc
Adam Ahrens <adam.aahrens@gmail.com>
2016-09-14 21:33:29 -0500

    Preventing registration for new Users

--- overtime/spec/factories/posts.rb
+++ overtime/spec/factories/posts.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  date       :date
+#  rationale  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 FactoryGirl.define do
   factory :first_post, class: Post do
     rationale "I don't feel well"
