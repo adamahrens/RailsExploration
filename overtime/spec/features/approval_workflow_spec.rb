a5c258a5aec64681128ce51d9f10d1d9aa4af473
Adam Ahrens <adam.aahrens@gmail.com>
2016-10-11 21:55:17 -0500

    Blocking setting status to only AdminUsers on Edit page

--- overtime/spec/features/approval_workflow_spec.rb
+++ overtime/spec/features/approval_workflow_spec.rb
@@ -9,14 +9,25 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post1 = FactoryGirl.create(:first_post)
+      visit edit_post_path(@post1)
     end
 
-    it 'has a status that can be edited on the form' do
-      visit edit_post_path(@post1)
+    it 'has a status that can be edited on the form by an admin user' do
       choose('post_status_approved')
       click_on 'Save'
       # Call reload after database call for latest info
       expect(@post1.reload.status).to eq('approved')
+    end
+
+    it 'cant be edited by a regular user' do
+      logout(:user)
+      regular_user = FactoryGirl.create(:user)
+      login_as(regular_user, scope: :user)
+      visit edit_post_path(@post1)
+
+      expect(page).to_not have_content('Approved')
+      expect(page).to_not have_content('Submitted')
+      expect(page).to_not have_content('Rejected')
     end
   end
 end
