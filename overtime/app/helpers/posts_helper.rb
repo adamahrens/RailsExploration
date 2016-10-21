a770dacda8f44a0ecc8811d26f4818b9da52293c
Adam Ahrens <adam.aahrens@gmail.com>
2016-10-20 21:46:56 -0500

    Hide actions for approved posts. Added helper for generating status span

--- overtime/app/helpers/posts_helper.rb
+++ overtime/app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def span_for_status(status)
+    case status
+    when 'approved'
+      return content_tag(:span, status.capitalize, class: 'label label-success')
+    when 'rejected'
+      return content_tag(:span, status.capitalize, class: 'label label-danger')
+    else
+      return content_tag(:span, status.capitalize, class: 'label label-primary')
+    end
+  end
 end
