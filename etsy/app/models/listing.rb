4bfcd05dd18fa4315b62814f70e2a9cbf5e0c33b
Adam Ahrens <adam.aahrens@gmail.com>
2017-03-27 19:48:07 -0500

    Adding the ability to upload images

--- etsy/app/models/listing.rb
+++ etsy/app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ApplicationRecord
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "Missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- etsy/db/schema.rb
+++ etsy/db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322200938) do
+ActiveRecord::Schema.define(version: 20170328002605) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
