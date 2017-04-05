8a31a042aade977cf1c0090c64a01c9610079023
Adam Ahrens <adam.aahrens@gmail.com>
2017-04-05 10:01:54 -0500

    Using annotate gem for routes and models

--- etsy/app/models/listing.rb
+++ etsy/app/models/listing.rb
@@ -1,10 +1,26 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  description         :text
+#  price               :decimal(, )
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
 class Listing < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "400x400>", thumb: "400x400>" }, default_url: "Missing.png",
                     storage: :s3,
                     s3_region: Rails.application.secrets.aws_region,
                     s3_host_name: "s3.amazonaws.com",
                     url: ":s3_host_name",
-                    path: "blah/#{Time.now.to_s}",
+                    path: "images/#{Time.now.to_s}",
                     s3_credentials: proc { |a| a.instance.s3_credentials }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
--- etsy/config/routes.rb
+++ etsy/config/routes.rb
@@ -1,3 +1,19 @@
+# == Route Map
+#
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     listings GET    /listings(.:format)          listings#index
+#              POST   /listings(.:format)          listings#create
+#  new_listing GET    /listings/new(.:format)      listings#new
+# edit_listing GET    /listings/:id/edit(.:format) listings#edit
+#      listing GET    /listings/:id(.:format)      listings#show
+#              PATCH  /listings/:id(.:format)      listings#update
+#              PUT    /listings/:id(.:format)      listings#update
+#              DELETE /listings/:id(.:format)      listings#destroy
+#         root GET    /                            listings#index
+#        about GET    /about(.:format)             pages#about
+#      contact GET    /contact(.:format)           pages#contact
+#
+
 Rails.application.routes.draw do
   resources :listings
   root 'listings#index'
