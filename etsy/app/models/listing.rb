518aacda2e95c34fc1f45aa154ee8e2cb2cbd3d8
Adam Ahrens <adam.aahrens@gmail.com>
2017-04-05 09:31:07 -0500

    Fixing path for image on aws

--- etsy/app/models/listing.rb
+++ etsy/app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "Missing.png",
                     storage: :s3,
                     s3_region: Rails.application.secrets.aws_region,
-                    s3_host_name: "s3-#{Rails.application.secrets.aws_region}.amazonaws.com",
+                    s3_host_name: "s3.amazonaws.com",
                     url: ":s3_host_name",
                     path: "blah/#{Time.now.to_s}",
                     s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
