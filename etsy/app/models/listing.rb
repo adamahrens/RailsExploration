36621dac8d5dbd5db66154ff8a41b22f0cf5af45
Adam Ahrens <adam.aahrens@gmail.com>
2017-04-05 09:55:01 -0500

    Updating size of images

--- etsy/app/models/listing.rb
+++ etsy/app/models/listing.rb
@@ -1,11 +1,11 @@
 class Listing < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "Missing.png",
+  has_attached_file :avatar, styles: { medium: "400x400>", thumb: "400x400>" }, default_url: "Missing.png",
                     storage: :s3,
                     s3_region: Rails.application.secrets.aws_region,
                     s3_host_name: "s3.amazonaws.com",
                     url: ":s3_host_name",
                     path: "blah/#{Time.now.to_s}",
-                    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
+                    s3_credentials: proc { |a| a.instance.s3_credentials }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def s3_credentials
