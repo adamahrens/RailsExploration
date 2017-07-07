334e70cce943b85dbbfd42122c1b2539af5ec594
Adam Ahrens <adam.aahrens@gmail.com>
2017-07-06 19:03:26 -0500

    Adding Devise User

--- Scheduler/config/routes.rb
+++ Scheduler/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static/homepage'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static#homepage'
 end
