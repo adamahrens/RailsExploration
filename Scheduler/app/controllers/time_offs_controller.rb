a2cda84ff51d0b64c34d2ead152dd49fcd0a649b
Adam Ahrens <adam.aahrens@gmail.com>
2017-08-09 15:30:59 -0500

    Adding the ability to create a Time Off Request

--- Scheduler/app/controllers/time_offs_controller.rb
+++ Scheduler/app/controllers/time_offs_controller.rb
@@ -1,7 +1,30 @@
 class TimeOffsController < ApplicationController
+  before_action :find_time_off, only: [:show]
+
   def index; end
 
   def new
     @timeoff = TimeOff.new
+  end
+
+  def create
+    @timeoff = TimeOff.create(time_off_params)
+    if @timeoff.save
+      redirect_to time_off_path(@timeoff), notice: 'Time Off Request saved'
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def find_time_off
+    @timeoff = TimeOff.find(params[:id])
+  end
+
+  def time_off_params
+    params.require(:time_off).permit(:rationale, :date)
   end
 end
