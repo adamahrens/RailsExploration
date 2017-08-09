49a8a2902690dafbe64dbd3de588ed1cc0acfebb
Adam Ahrens <adam.aahrens@gmail.com>
2017-08-09 15:04:26 -0500

    Adding basic template

--- Scheduler/app/controllers/time_offs_controller.rb
+++ Scheduler/app/controllers/time_offs_controller.rb
@@ -1,3 +1,7 @@
 class TimeOffsController < ApplicationController
   def index; end
+
+  def new
+    @timeoff = TimeOff.new
+  end
 end
--- Scheduler/spec/features/time_off_spec.rb
+++ Scheduler/spec/features/time_off_spec.rb
@@ -12,4 +12,21 @@ describe 'navigation' do
       expect page.has_content?('Time Off Requests')
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_time_off_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can submit a Time Off Request' do
+      visit new_time_off_path
+      rationale = 'Testing time off'
+      now = Date.today
+      fill_in 'time_off[rationale]', with: rationale
+      fill_in 'time_off[date]', with: now
+      click_on 'Save'
+      expect page.has_content?(rationale)
+    end
+  end
 end
