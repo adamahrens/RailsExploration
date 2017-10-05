5998b877ca0c90cc8817bcce9a61cf6ad1c63952
Adam Ahrens <adam.aahrens@gmail.com>
2017-10-04 21:25:31 -0500

    Removing ability to register

--- Scheduler/config/routes.rb
+++ Scheduler/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :time_offs
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   get 'static/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static#homepage'
