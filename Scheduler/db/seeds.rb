7c5e05535f7dd0b3027c7376547d36ca05834740
Adam Ahrens <adam.aahrens@gmail.com>
2018-02-11 20:47:31 -0600

    Updating seeds data

--- Scheduler/db/seeds.rb
+++ Scheduler/db/seeds.rb
@@ -6,19 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(email: 'first@last.com',
-  first_name: 'First',
-  last_name: 'Last',
+adam = User.create(email: 'a@a.com',
+  first_name: 'Adam',
+  last_name: 'Ahrens',
   password: 'password123',
   password_confirmation: 'password123',
-phoneNumber: '6514085145')
+phoneNumber: '9119112222')
 
-10.times do |index|
+claudia = User.create(email: 'b@b.com',
+  first_name: 'Claudia',
+  last_name: 'Ahrens',
+  password: 'password123',
+  password_confirmation: 'password123',
+phoneNumber: '9119113333')
+
+admin = AdminUser.create(email: 'admin@admin.com',
+  first_name: 'Admin',
+  last_name: 'Ahrens',
+  password: 'password123',
+  password_confirmation: 'password123',
+phoneNumber: '9119114444')
+
+5.times do |index|
   now = Date.today
   time_ago = now - index
-  TimeOff.create(date: time_ago, rationale: "#{index} Going on vacation", overtime_request: 0.5, user_id: user.id)
+  TimeOff.create(date: time_ago, rationale: "#{index} Going on vacation", overtime_request: 0.5, user_id: adam.id)
 end
 
-10.times do |index|
-  AuditLog.create(user_id: user.id, status: 0, start_date: (Date.today - 6.days))
+5.times do |index|
+  now = Date.today
+  time_ago = now - index
+  TimeOff.create(date: time_ago, rationale: "#{index} Going on vacation", overtime_request: 0.5, user_id: claudia.id)
+end
+
+5.times do |index|
+  AuditLog.create(user_id: adam.id)
+end
+
+5.times do |index|
+  AuditLog.create(user_id: claudia.id)
 end
