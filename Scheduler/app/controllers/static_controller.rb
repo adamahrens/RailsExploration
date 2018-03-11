9dde166b9b062d471d817a5606fc9406040e0549
Adam Ahrens <adam.aahrens@gmail.com>
2018-03-11 18:11:23 -0500

    Adding audit log for homepage

--- Scheduler/app/controllers/static_controller.rb
+++ Scheduler/app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def homepage
     @pending_approvals = TimeOff.all.where(status: 0).includes(:user)
+    @recents = AuditLog.all.order(:created_at).limit(10).includes(:user)
   end
 end
