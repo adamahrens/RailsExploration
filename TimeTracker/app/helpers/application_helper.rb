18f289a0b64ac1796cd31c09db08cccb6eb5a39f
Adam Ahrens <adam.aahrens@gmail.com>
2018-03-27 17:34:49 -0500

    Configuring Header with Javascript to draw link text

--- TimeTracker/app/helpers/application_helper.rb
+++ TimeTracker/app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  # List of nav items
+  def nav_items
+    [
+      { display_text: 'Companies',
+        controller_name: 'companies',
+      path: companies_path },
+      { display_text: 'Projects',
+        controller_name: 'projects',
+      path: projects_path },
+      { display_text: 'Works',
+        controller_name: 'works',
+      path: works_path }
+    ]
+  end
+
+  # Displays nav item
+  def display_nav_item(display_text, controller, link_url)
+    css_class = controller_name == controller ? 'class="active"' : ''
+    link = link_to display_text, link_url
+    raw("<li #{css_class}>#{link}</li>")
+  end
 end
