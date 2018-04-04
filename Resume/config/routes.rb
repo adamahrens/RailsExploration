a0818a7fbfe3a3b95caa883270437bbee379dbb4
Adam Ahrens <adam.aahrens@gmail.com>
2018-04-04 08:50:00 -0500

    Running annotate and adding a rake task

--- Resume/config/routes.rb
+++ Resume/config/routes.rb
@@ -1,3 +1,16 @@
+# == Route Map
+#
+#    Prefix Verb   URI Pattern               Controller#Action
+#     blogs GET    /blogs(.:format)          blogs#index
+#           POST   /blogs(.:format)          blogs#create
+#  new_blog GET    /blogs/new(.:format)      blogs#new
+# edit_blog GET    /blogs/:id/edit(.:format) blogs#edit
+#      blog GET    /blogs/:id(.:format)      blogs#show
+#           PATCH  /blogs/:id(.:format)      blogs#update
+#           PUT    /blogs/:id(.:format)      blogs#update
+#           DELETE /blogs/:id(.:format)      blogs#destroy
+# 
+
 Rails.application.routes.draw do
   resources :blogs
 end
