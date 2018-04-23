674ddbd830cdda889d6907cc8556c4e634ea931d
Adam Ahrens <adam.aahrens@gmail.com>
2018-04-22 19:50:26 -0500

    Tweaking devise registration form to include custom attributes from User

--- TimeTracker/app/models/user.rb
+++ TimeTracker/app/models/user.rb
@@ -14,12 +14,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :company
   has_many :works
   has_many :projects, through: :works
 
-  validates :first_name, :last_name, presence: true
-  validates :company, presence: true
+  validates :first_name, :last_name, :company, presence: true
   validates :first_name, length: { minimum: 2 }
   validates :last_name, length: { minimum: 5 }
 
