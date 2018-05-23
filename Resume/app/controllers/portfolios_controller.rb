3dc3cc4a3ecefef8bd443fecc856764ed6b88bdf
Adam Ahrens <adam.aahrens@gmail.com>
2018-05-23 10:49:22 -0500

    Adding the ability to create a new Portfolio

--- Resume/app/controllers/portfolios_controller.rb
+++ Resume/app/controllers/portfolios_controller.rb
@@ -2,4 +2,29 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        #format.html { redirect_to @portfolio, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :image, :thumbnail)
+  end
 end
