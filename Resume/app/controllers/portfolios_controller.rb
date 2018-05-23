3fca0e510a108cd5e5b89d1eea267ae9932b0352
Adam Ahrens <adam.aahrens@gmail.com>
2018-05-23 11:08:12 -0500

    Adding ability to update a portfolio

--- Resume/app/controllers/portfolios_controller.rb
+++ Resume/app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[edit update]
   def index
     @portfolios = Portfolio.all
   end
@@ -22,7 +23,27 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        #format.html { redirect_to @portfolio, notice: 'Portfolio was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, :image, :thumbnail)
