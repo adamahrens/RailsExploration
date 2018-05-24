65af3dff65c627c408c8a3a9b4e45bfbc43741c3
Adam Ahrens <adam.aahrens@gmail.com>
2018-05-24 11:39:52 -0500

    Added some validation and duplicate gemfile resource

--- Resume/app/models/blog.rb
+++ Resume/app/models/blog.rb
@@ -8,7 +8,6 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Blog < ApplicationRecord
   validates :title, :body, presence: true
 end
--- Resume/app/models/portfolio.rb
+++ Resume/app/models/portfolio.rb
@@ -11,6 +11,6 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Portfolio < ApplicationRecord
+  validates :title, :subtitle, :body, presence: true
 end
