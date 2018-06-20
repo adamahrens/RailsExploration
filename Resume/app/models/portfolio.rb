60fbdf03fdffaca627af997de5480d535d2a6c42
Adam Ahrens <adam.aahrens@gmail.com>
2018-06-20 14:09:58 -0500

    Adding default image & thumbnail for Portfolio

--- Resume/app/models/portfolio.rb
+++ Resume/app/models/portfolio.rb
@@ -14,9 +14,19 @@
 class Portfolio < ApplicationRecord
   validates :title, :subtitle, :body, :image, :thumbnail, presence: true
 
+  # Run after Model.new
+  after_initialize :set_defaults
+
   scope :designers, -> { where(title: 'designer') }
 
   def self.designer
     where(title: 'designer')
+  end
+
+  def set_defaults
+    # set an image if it is null
+    # useful if defaults arent set via a migration
+    self.image ||= 'http://via.placeholder.com/300x300'
+    self.thumbnail ||= 'http://via.placeholder.com/140x100'
   end
 end
