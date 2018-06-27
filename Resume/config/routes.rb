401f6d1e5844ed2abea77a25601bde80560edea3
Adam Ahrens <adam.aahrens@gmail.com>
2018-06-27 14:59:42 -0500

    Add ability to login, logout, and register

--- Resume/config/routes.rb
+++ Resume/config/routes.rb
@@ -23,7 +23,9 @@
 #        contact GET    /contact(.:format)             pages#contact
 #           root GET    /                              pages#home
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login',
+                                             sign_out: 'logout',
+                                             sign_up: 'register' }
   resources :portfolios, except: [:show]
   # adds show_portfolio_path
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
