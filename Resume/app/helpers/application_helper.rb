eb33446130534f3dda5d56f40d405bfe157e7b98
Adam Ahrens <adam.aahrens@gmail.com>
2018-07-10 13:49:41 -0500

    Adding view helpers to conditionally generate html to display, removing from view

--- Resume/app/helpers/application_helper.rb
+++ Resume/app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
+# Login logic
 module ApplicationHelper
+  def login_links_helper
+    if current_user.is_a?(User) && user_signed_in?
+      logout = link_to 'Logout', destroy_user_session_path, method: :delete
+      content_tag :div, logout
+    else
+      login = link_to 'Login', new_user_session_path
+      register = link_to 'Register', new_user_registration_path
+      content_tag :div do
+        [login, register].join(' | ').html_safe
+      end
+    end
+  end
+
+  def source_helper
+    return unless session[:source]
+    content_tag :div do
+      hr = content_tag(:hr)
+      p = content_tag(:p, "Thanks for coming from #{session[:source]}")
+      hr + p
+    end
+  end
 end
