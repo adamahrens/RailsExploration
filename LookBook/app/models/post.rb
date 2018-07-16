511cc1aa02a3ae752ddde1526d0eeb3960e40837
Adam Ahrens <adam.aahrens@gmail.com>
2018-07-16 15:44:01 -0500

    Running annotate

--- LookBook/app/models/post.rb
+++ LookBook/app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
   validates :title, :content, presence: true
 end
--- LookBook/config/routes.rb
+++ LookBook/config/routes.rb
@@ -1,3 +1,18 @@
+# == Route Map
+#
+#    Prefix Verb   URI Pattern               Controller#Action
+#     posts GET    /posts(.:format)          posts#index
+#           POST   /posts(.:format)          posts#create
+#  new_post GET    /posts/new(.:format)      posts#new
+# edit_post GET    /posts/:id/edit(.:format) posts#edit
+#      post GET    /posts/:id(.:format)      posts#show
+#           PATCH  /posts/:id(.:format)      posts#update
+#           PUT    /posts/:id(.:format)      posts#update
+#           DELETE /posts/:id(.:format)      posts#destroy
+#      home GET    /home(.:format)           pages#home
+#      root GET    /                         pages#home
+# 
+
 Rails.application.routes.draw do
   resources :posts
   get 'home', to: 'pages#home'
