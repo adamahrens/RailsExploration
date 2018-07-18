ed86ee226e09929f388d7c6d690a1302624c66eb
Adam Ahrens <adam.aahrens@gmail.com>
2018-07-18 08:33:07 -0500

    Added roles for Petergate to User

--- Resume/app/models/user.rb
+++ Resume/app/models/user.rb
@@ -14,6 +14,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  roles                  :string
 #  sign_in_count          :integer          default(0), not null
 #  updated_at             :datetime         not null
 #
@@ -24,6 +25,15 @@
 #
 
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  ############################################################################################ 
+ 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- Resume/config/routes.rb
+++ Resume/config/routes.rb
@@ -1,28 +1,44 @@
 # == Route Map
 #
-#         Prefix Verb   URI Pattern                    Controller#Action
-#     portfolios GET    /portfolios(.:format)          portfolios#index
-#                POST   /portfolios(.:format)          portfolios#create
-#  new_portfolio GET    /portfolios/new(.:format)      portfolios#new
-# edit_portfolio GET    /portfolios/:id/edit(.:format) portfolios#edit
-#      portfolio PATCH  /portfolios/:id(.:format)      portfolios#update
-#                PUT    /portfolios/:id(.:format)      portfolios#update
-#                DELETE /portfolios/:id(.:format)      portfolios#destroy
-# show_portfolio GET    /portfolio/:id(.:format)       portfolios#show
-# publicize_blog POST   /blogs/:id/publicize(.:format) blogs#publicize
-#          blogs GET    /blogs(.:format)               blogs#index
-#                POST   /blogs(.:format)               blogs#create
-#       new_blog GET    /blogs/new(.:format)           blogs#new
-#      edit_blog GET    /blogs/:id/edit(.:format)      blogs#edit
-#           blog GET    /blogs/:id(.:format)           blogs#show
-#                PATCH  /blogs/:id(.:format)           blogs#update
-#                PUT    /blogs/:id(.:format)           blogs#update
-#                DELETE /blogs/:id(.:format)           blogs#destroy
-#           home GET    /home(.:format)                pages#home
-#          about GET    /about(.:format)               pages#about
-#        contact GET    /contact(.:format)             pages#contact
-#           root GET    /                              pages#home
-Rails.application.routes.draw do
+#                   Prefix Verb   URI Pattern                    Controller#Action
+#         new_user_session GET    /login(.:format)               devise/sessions#new
+#             user_session POST   /login(.:format)               devise/sessions#create
+#     destroy_user_session DELETE /logout(.:format)              devise/sessions#destroy
+#        new_user_password GET    /password/new(.:format)        devise/passwords#new
+#       edit_user_password GET    /password/edit(.:format)       devise/passwords#edit
+#            user_password PATCH  /password(.:format)            devise/passwords#update
+#                          PUT    /password(.:format)            devise/passwords#update
+#                          POST   /password(.:format)            devise/passwords#create
+# cancel_user_registration GET    /cancel(.:format)              devise/registrations#cancel
+#    new_user_registration GET    /register(.:format)            devise/registrations#new
+#   edit_user_registration GET    /edit(.:format)                devise/registrations#edit
+#        user_registration PATCH  /                              devise/registrations#update
+#                          PUT    /                              devise/registrations#update
+#                          DELETE /                              devise/registrations#destroy
+#                          POST   /                              devise/registrations#create
+#               portfolios GET    /portfolios(.:format)          portfolios#index
+#                          POST   /portfolios(.:format)          portfolios#create
+#            new_portfolio GET    /portfolios/new(.:format)      portfolios#new
+#           edit_portfolio GET    /portfolios/:id/edit(.:format) portfolios#edit
+#                portfolio PATCH  /portfolios/:id(.:format)      portfolios#update
+#                          PUT    /portfolios/:id(.:format)      portfolios#update
+#                          DELETE /portfolios/:id(.:format)      portfolios#destroy
+#           show_portfolio GET    /portfolio/:id(.:format)       portfolios#show
+#           publicize_blog POST   /blogs/:id/publicize(.:format) blogs#publicize
+#                    blogs GET    /blogs(.:format)               blogs#index
+#                          POST   /blogs(.:format)               blogs#create
+#                 new_blog GET    /blogs/new(.:format)           blogs#new
+#                edit_blog GET    /blogs/:id/edit(.:format)      blogs#edit
+#                     blog GET    /blogs/:id(.:format)           blogs#show
+#                          PATCH  /blogs/:id(.:format)           blogs#update
+#                          PUT    /blogs/:id(.:format)           blogs#update
+#                          DELETE /blogs/:id(.:format)           blogs#destroy
+#                     home GET    /home(.:format)                pages#home
+#                    about GET    /about(.:format)               pages#about
+#                  contact GET    /contact(.:format)             pages#contact
+#                     root GET    /                              pages#home
+# 
+
   devise_for :users, path: '', path_names: { sign_in: 'login',
                                              sign_out: 'logout',
                                              sign_up: 'register' }
