b1e8df552da284e84ee6f9c654a549eef3312b12
Adam Ahrens <adam.aahrens@gmail.com>
2018-08-17 16:15:16 -0500

    Tweaking Portfolio layout

--- Resume/app/controllers/portfolios_controller.rb
+++ Resume/app/controllers/portfolios_controller.rb
@@ -6,7 +6,7 @@ class PortfoliosController < ApplicationController
          site_admin: :all
 
   def index
-    @portfolios = Portfolio.all
+    @portfolios = Portfolio.all.order(created_at: 'desc')
     @page_title += ' | My Portfolio'
     # Custom scope usage
     # @portfolios = Portfolio.designers
