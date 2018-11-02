daa1fcf24882312498030fc0d41ec206c3b657f0
Adam Ahrens <adam.aahrens@gmail.com>
2018-11-02 12:30:52 -0500

    Removing old project

--- LookBook/db/migrate/20180717175448_add_nickname_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddNicknameToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :nickname, :string
-    add_column :users, :description, :text
-    add_column :users, :oauth_secret, :string
-    add_column :users, :image, :string
-  end
-end
