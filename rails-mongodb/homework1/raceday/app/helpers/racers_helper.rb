f4e4737787ea288d2958355a6ff41ed2f21e3dee
Adam Ahrens <adam.aahrens@gmail.com>
2018-11-02 11:18:51 -0500

    Deleting old stuff

--- rails-mongodb/homework1/raceday/app/helpers/racers_helper.rb
+++
@@ -1,5 +0,0 @@
-module RacersHelper
-  def convert_racer(racer)
-    racer.is_a?(Racer) ? racer : Racer.new(racer)
-  end
-end
--- rails-mongodb/homework1/raceday/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  resources :racers
-  root 'racers#index'
-end
